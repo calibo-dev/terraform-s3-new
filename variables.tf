5e8747c4eb640516ee2c2789fa27d6d813cad644
software-license-calibo <software-license@calibo.com>
2025-07-10 14:24:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@ variable "s3_bucket_name" {
 
 variable "test_1" {
   default = ""
+  sensitive = true
 }
 
 variable "test_2" {
@@ -13,3 +14,12 @@ variable "test_2" {
 variable "test_3" {
   default = ""
 }
+
+output "test_11" {
+   default = var.test_1
+}
+
+output "test_12" {
+   default = var.test_2
+}
+
