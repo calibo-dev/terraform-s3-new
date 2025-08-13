10a247dbcdd6f7f3ae96f38525e4b4beca11347d
software-license-calibo <software-license@calibo.com>
2025-08-13 12:06:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket     = "backend-bucket-new"
-    region     = "us-east-1"
-    encrypt    = true
-    key        = "BUILD_NUM/terraform.tfstate"
-  }
-}
 
-provider "aws" {
-  region = "us-east-1"
-}
