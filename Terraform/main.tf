e4d4c2a3c023f898b9bd848a8816af4879262c43
Abhijeet kamble <akamble@calibo.com>
2025-08-14 18:32:49 +0530

    added changes for Terraform folder

---
+++ Terraform/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.s3_bucket_name
+
+  tags = {
+    Name        = var.s3_bucket_name
+    Environment = "Dev"
+    Test-1      = var.test_1
+    Test-2      = var.test_2
+    Test-3      = var.test_3
+  }
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "s3_bucket_name" {
+  default = ""
+}
+
+variable "test_1" {
+  default = ""
+  sensitive = true
+}
+
+variable "test_2" {
+  default = ""
+}
+
+variable "test_3" {
+  default = ""
+}
+
+output "test_11" {
+   value = var.test_1
+   sensitive = true
+}
+
+output "test_12" {
+   value = var.test_2
+}
+
